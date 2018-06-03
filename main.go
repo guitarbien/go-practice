809f51a52e16830498ea45dc99d6d391eedb22bc
guitarbien <guitarbien@gmail.com>
2018-06-03 19:28:14 +0800

    multiple return

--- main.go
+++ main.go
@@ -18,11 +18,21 @@ func add(i, j int) int {
   return i + j
 }
 
+func swap(i, j int) (int, int) {
+  return j, i
+}
+
 func main() {
   foo := "Hello"
   bar := 100
 
   fmt.Println(add(2, 3))
+
+  a := 1
+  b := 2
+  a, b = swap(a, b)
+  fmt.Println("a:", a)
+  fmt.Println("b:", b)
 
   fmt.Println(foo)
   fmt.Println(bar)
