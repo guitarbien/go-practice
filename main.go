bb90f9c6049514c3898756fcdaa1cb52d7c9cd75
guitarbien <guitarbien@gmail.com>
2018-06-03 19:16:36 +0800

    const 宣告

--- main.go
+++ main.go
@@ -4,6 +4,16 @@ import (
   "fmt"
 )
 
+const (
+  Monday    = 1
+  Tuesday   = 2
+  Wednesday = 3
+  Thursday  = 4
+  Friday    = 5
+  Saturday  = 6
+  Sunday    = 7
+)
+
 func main() {
   foo := "Hello"
   bar := 100
@@ -11,4 +21,12 @@ func main() {
   fmt.Println(foo)
   fmt.Println(bar)
   fmt.Println("done !!")
+
+  fmt.Println(Monday)
+  fmt.Println(Tuesday)
+  fmt.Println(Wednesday)
+  fmt.Println(Thursday)
+  fmt.Println(Friday)
+  fmt.Println(Saturday)
+  fmt.Println(Sunday)
 }
