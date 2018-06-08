1b0d8902dd8e3599b0853bf1c62ca2f8707394ad
guitarbien <guitarbien@gmail.com>
2018-06-08 22:01:39 +0800

    減少 call by reference 的操作，直接回傳 value 即可

--- slice.go
+++ slice.go
@@ -3,15 +3,16 @@ package main
 import "fmt"
 
 // pass slice as function argument
-func addValue(foo *[]string)  {
-  *foo = append(*foo, "c")
+func addValue(foo []string) []string {
+  foo = append(foo, "c")
   fmt.Println("addValue foo", foo)
+  return foo
 }
 
 func main()  {
   foo := []string{"a", "b"}
   fmt.Println("before foo:", foo)
 
-  addValue(&foo)
+  foo = addValue(foo)
   fmt.Println("after foo:", foo)
 }
