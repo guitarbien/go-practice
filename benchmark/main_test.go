d92d0d4b7c0495ec1c8214f93cf28c8137f398fc
guitarbien <guitarbien@gmail.com>
2018-07-13 23:00:03 +0800

    以 Benchmark 開頭的 func 測試效能
    b.N 為一個未知數，go 預設會執行每個 func 的無窮迴圈一秒鐘，來看一秒鐘內能執行多少次

--- benchmark/main_test.go
+++ benchmark/main_test.go
@@ -19,3 +19,21 @@ func TestPrint03(t *testing.T)  {
     t.Fatal("error")
   }
 }
+
+func BenchmarkPrint01(b *testing.B)  {
+  for i:=0; i < b.N; i++ {
+    print01(100)
+  }
+}
+
+func BenchmarkPrint02(b *testing.B)  {
+  for i:=0; i < b.N; i++ {
+    print02(int64(100))
+  }
+}
+
+func BenchmarkPrint03(b *testing.B)  {
+  for i:=0; i < b.N; i++ {
+    print03(100)
+  }
+}
